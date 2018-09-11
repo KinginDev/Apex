bd72f6168c2802d29819836c26a82caef41f8533
genmax100.ab@gmail.com <genmax100.ab@gmail.com>
2018-09-10 18:19:20 -0700

    Partners

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Paystack;
 use App\Donation;
+use Session;
 class PaymentController extends Controller
 {
     public function view(){
