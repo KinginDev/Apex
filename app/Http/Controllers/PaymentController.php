a32e98bc11ae986028110d0b84e033eaa25965fc
genmax100.ab@gmail.com <genmax100.ab@gmail.com>
2018-09-10 17:43:24 -0700

    Partners

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -24,10 +24,10 @@ class PaymentController extends Controller
     {
         $paymentDetails = Paystack::getPaymentData();
         $data = collect($paymentDetails);
-        dd($data);
+        
                 $amount =new Donation();
-                $amount->email = $data->data->customer->email;
-                $amount->amount = $data->data->amount;
+                $amount->email = $data[data[customer[email]]];
+                $amount->amount = $data[data[amount]];
                 $amount->save();
                 Session::flash('success', 'Payment successfully made thank you for dropping by!');
                 return redirect(route('pay.view'));
