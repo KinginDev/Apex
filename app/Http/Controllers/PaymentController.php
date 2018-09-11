43df6a49c270e8d7fc63320d77d64400c36b560c
genmax100.ab@gmail.com <genmax100.ab@gmail.com>
2018-09-10 17:53:17 -0700

    Partners

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -26,8 +26,8 @@ class PaymentController extends Controller
         $data = collect($paymentDetails);
         dd($data->data);
                 $amount =new Donation();
-                $amount->email = $data->data[customer[email]]];
-                $amount->amount = $data->data[amount]];
+                $amount->email = $data->data[customer[email]];
+                $amount->amount = $data->data[amount];
                 $amount->save();
                 Session::flash('success', 'Payment successfully made thank you for dropping by!');
                 return redirect(route('pay.view'));
